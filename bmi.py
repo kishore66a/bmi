2bd992e98c20e239f410ada20cfb868342bba907
srinu9909 <rkasi5796@gmail.com>
2024-06-21 11:53:08 +0530

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,10 +1,7 @@
 the_height = float(input("Enter the height in cm: "))  
 the_weight = float(input("Enter the weight in kg: "))  
-# defining a function for BMI  
-the_BMI = the_weight / (the_height/100)**2  
-# printing the BMI  
-print("Your Body Mass Index is", the_BMI)  
-# using the if-elif-else conditions  
+the_BMI = the_weight / (the_height/100)**2
+print("Your Body Mass Index is", the_BMI) 
 if the_BMI <= 18.5:  
     print("Oops! You are underweight.")  
 elif the_BMI <= 24.9:  
